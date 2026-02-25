8f7b1d85c1d3334db9e0da223e0ed8aee913c3c8
CestMoi77 <102248291+CestMoi77@users.noreply.github.com>
2026-02-25 03:22:41 +0100

    Added _BlueprintLayout and project sync

--- src/EternalKids.Web/obj/Debug/net8.0/EternalKids.Web.AssemblyInfo.cs
+++ src/EternalKids.Web/obj/Debug/net8.0/EternalKids.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EternalKids.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5e595f403b42592e462c91d99df55c2fce164ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57862ba84076d135589fe1a75c81d4142a93f4bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("EternalKids.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EternalKids.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
